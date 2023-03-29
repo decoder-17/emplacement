90b0ea19a7e28877c188112ebde92ff924f212c7
coder_17 <tanupamsaha26@gmail.com>
2023-03-29 23:26:35 +0530

    file-structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/layout/Navbar'
 import { Hero } from './components/sections/Hero'
+import About from './components/About'
+import Contact from './components/Contact'
 function App() {
 
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
+      <Contact />
     </div>
   )
 }
