c6847d97c21a97461099de6a41f660b83fe00bd4
coder_17 <tanupamsaha26@gmail.com>
2023-03-31 02:34:19 +0530

    addded introduction

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Navbar } from './components/layout/Navbar'
 import { Hero } from './components/sections/Hero'
 import About from './components/About'
 import Contact from './components/Contact'
+import Introduction from './components/sections/Introduction'
 function App() {
 
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Introduction />
       <About />
       <Contact />
     </div>
