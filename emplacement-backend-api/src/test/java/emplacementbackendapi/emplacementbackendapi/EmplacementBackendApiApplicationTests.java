e26ce63bcc1ee75c7485e51deff9640780ae6c51
Neeraj kumar <neerajkr1210@gmail.com>
2023-04-01 00:24:00 +0530

    🚀 Added Docker For deployment - Backend Added

---
+++ emplacement-backend-api/src/test/java/emplacementbackendapi/emplacementbackendapi/EmplacementBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package emplacementbackendapi.emplacementbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplacementBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
