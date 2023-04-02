d56a31eabe558ddf3d821f59218b507a0ceb5e51
coder_17 <tanupamsaha26@gmail.com>
2023-04-02 16:23:51 +0530

    minor changes, not enough for a push

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import '../assets/css/hero.css'
 
 export default function Auth() {
+
   return (
-    <div>
+    <div className='signin'>
       <input type='text' placeholder='Email goes here' />
       <inupt type='password' placeholder='Enter your password' />
-      <button type='submit'>Sing In</button>
+      <button type='submit'>Sign In</button>
     </div>
   )
 }
