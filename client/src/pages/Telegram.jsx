3de36b6385afb4295f6c3b16dc2e5c60ea5ce707
coder_17 <tanupamsaha26@gmail.com>
2023-04-03 00:43:01 +0530

    Telegram page updated

--- client/src/pages/Telegram.jsx
+++ client/src/pages/Telegram.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../assets/css/telegram.css'
+import telegram from '../assets/images/telegram.svg'
 
 export default function Telegram() {
   return (
-    <div>
-      Add to your Telegram
+    <div className='telegram'>
+      <div className="heading">
+        Emplacement is now available on Telegram.
+      </div>
+      <div className="body">
+        <p>Things are easier than before. You can now get all the latest updates on your Telegram. Turn on your notifications and stay updated.</p>
+      </div>
+      <div className='telegrambtn'>
+        <button className="popbutton">
+
+          Telegram<img src={telegram} className='popuplogo' /></button>
+      </div>
     </div>
   )
 }
