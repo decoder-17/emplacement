ea8f98b1156800e1477a05518d210ad5c50bc93d
coder_17 <tanupamsaha26@gmail.com>
2023-04-03 00:32:41 +0530

    Firebase auth setup

--- client/src/pages/Jobs.jsx
+++ client/src/pages/Jobs.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../assets/css/jobs.css'
 
 export default function Jobs() {
   return (
-    <div>
-      <h1>Jobs</h1>
+    <div className='jobs'>
+      <div className=" buttons"><button className='job-btn recent'>Recents</button>
+        <button className='job-btn search'>Search</button></div>
     </div>
   )
 }
