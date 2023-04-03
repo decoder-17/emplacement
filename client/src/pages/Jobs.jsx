cb681c209ef31574b6a4df4239990a86788b67d4
coder_17 <tanupamsaha26@gmail.com>
2023-04-03 11:50:53 +0530

    Search button

--- client/src/pages/Jobs.jsx
+++ client/src/pages/Jobs.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import '../assets/css/jobs.css'
+import React from "react";
+import "../assets/css/jobs.css";
 
 export default function Jobs() {
   return (
-    <div className='jobs'>
-      <div className=" buttons"><button className='job-btn recent'>Recents</button>
-        <button className='job-btn search'>Search</button></div>
+    <div className="jobs">
+      <div className="navigation">
+        <div className=" buttons">
+          <button className="job-btn recent">Recents</button>
+          <button className="job-btn search">Search</button>
+        </div>
+        <div >
+          <input type="text" className="search-btn search" placeholder="Search here..." autoFocus />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
