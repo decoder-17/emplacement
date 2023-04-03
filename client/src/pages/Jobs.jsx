ed33fac36ea9c0d59179c7df01a7d352b965bf6b
coder_17 <tanupamsaha26@gmail.com>
2023-04-04 00:02:02 +0530

    card-strcuture-creation

--- client/src/pages/Jobs.jsx
+++ client/src/pages/Jobs.jsx
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from "react";
 import "../assets/css/jobs.css";
 
 export default function Jobs() {
-
   const [jobs, setJobs] = useState([]);
-  const url = "https://backend-decoder-17.cloud.okteto.net/api/v1/site/cuvette/internships?page=1";
+  const url =
+    "https://backend-decoder-17.cloud.okteto.net/api/v1/site/cuvette/internships?page=1";
 
-  const fetchJobData = () => {
+  const fetchJobData = async () => {
     try {
-      fetch(url).then(response => {
-        return response.json()
-      }).then(data => {
-        setJobs(data)
-      })
+      await fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setJobs(data);
+        });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchJobData()
-  }, [])
+    fetchJobData();
+  }, []);
 
-
-  console.log(jobs)
   return (
     <div className="jobs">
       <div className="navigation">
@@ -31,18 +31,46 @@ export default function Jobs() {
           <button className="job-btn recent">Recents</button>
           <button className="job-btn search">Search</button>
         </div>
-        <div >
-          <input type="text" className="search-btn search" placeholder="Search here..." autoFocus />
+        <div>
+          <input
+            type="text"
+            className="search-btn search"
+            placeholder="Search here..."
+            autoFocus
+          />
         </div>
       </div>
       <div className="job-list">
-        {/* {jobs.length > 0 && (
-          <ul className="card">
-            {jobs.map(job => (
-              <li key={job.id} className="job-title">{job.internshipMode}</li>
+        {jobs.data && (
+          <div className="card">
+            {jobs.data.map((job) => (
+              <div key={job.id} className="job-info">
+                <div className="card-heading">
+                  <img
+                    src={job.refUser.refCompanyProfile.logoUrl}
+                    className="logo companylogo"
+                    alt="Company Logo"
+                  />
+                  <p className="companyname">
+                    {job.refUser.refCompanyProfile.companyName}
+                  </p>
+                </div>
+                <div className="companydetails">
+
+                  <p className="companylocation">
+                    Office Location: {job.refUser.refCompanyProfile.refLocation.state}
+                  </p>
+                  <p className="timeperiod">Time period: {job.duration}</p>
+                </div>
+                <div className="btnlist">
+                  <button className="button view">View More</button>
+                  <button className="button">Apply</button>
+                </div>
+              </div>
             ))}
-          </ul>)} */}
+          </div>
+        )}
       </div>
-    </div >
+    </div>
   );
 }
