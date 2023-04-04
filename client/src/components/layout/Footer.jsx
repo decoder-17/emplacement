1bac8061e7df23d62b5f7958b08541833332b16c
coder_17 <tanupamsaha26@gmail.com>
2023-04-04 20:53:18 +0530

    added footer and redesigned the cards

---
+++ client/src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer' >
+      Developed and Designed by <a href='' className='brand' >@coder_17</a>
+    </div>
+  )
+}
