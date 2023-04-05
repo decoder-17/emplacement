8b0e282b50380dd6ece6b03d6041f2e745e4bb7f
coder_17 <tanupamsaha26@gmail.com>
2023-04-05 10:20:46 +0530

    fixed telegram popup

---
+++ client/src/components/sections/CardSkeleton.jsx
@@ -0,0 +1,195 @@
+import React from 'react'
+import Skeleton from "react-loading-skeleton";
+
+export default function CardSkeleton() {
+  return (
+    <div className="card">
+      <div className="job-info">
+        <div className="card-heading">
+          <div className="header">
+            <p className="companyname">
+              <Skeleton />
+            </p>
+            <p className="job-desc">
+              <Skeleton />
+            </p>
+          </div>
+        </div>
+        <div className="companydetails">
+          <p className="companylocation info">
+            <Skeleton />
+          </p>
+          <p className="timeperiod info">
+            <Skeleton />
+          </p>
+          <p className="stipend info">
+            <Skeleton />
+          </p>
+        </div>
+        <div className="btnlist">
+          <button className="secondary-btn more">
+            View
+          </button>
+          <a className="secondary-btn apply" target="_blank">
+            Apply
+          </a>
+        </div>
+      </div>
+      <div className="job-info">
+        <div className="card-heading">
+          <div className="header">
+            <p className="companyname">
+              <Skeleton />
+            </p>
+            <p className="job-desc">
+              <Skeleton />
+            </p>
+          </div>
+        </div>
+        <div className="companydetails">
+          <p className="companylocation info">
+            <Skeleton />
+          </p>
+          <p className="timeperiod info">
+            <Skeleton />
+          </p>
+          <p className="stipend info">
+            <Skeleton />
+          </p>
+        </div>
+        <div className="btnlist">
+          <button className="secondary-btn more">
+            View
+          </button>
+          <a className="secondary-btn apply" target="_blank">
+            Apply
+          </a>
+        </div>
+      </div>
+      <div className="job-info">
+        <div className="card-heading">
+          <div className="header">
+            <p className="companyname">
+              <Skeleton />
+            </p>
+            <p className="job-desc">
+              <Skeleton />
+            </p>
+          </div>
+        </div>
+        <div className="companydetails">
+          <p className="companylocation info">
+            <Skeleton />
+          </p>
+          <p className="timeperiod info">
+            <Skeleton />
+          </p>
+          <p className="stipend info">
+            <Skeleton />
+          </p>
+        </div>
+        <div className="btnlist">
+          <button className="secondary-btn more">
+            View
+          </button>
+          <a className="secondary-btn apply" target="_blank">
+            Apply
+          </a>
+        </div>
+      </div>
+      <div className="job-info">
+        <div className="card-heading">
+          <div className="header">
+            <p className="companyname">
+              <Skeleton />
+            </p>
+            <p className="job-desc">
+              <Skeleton />
+            </p>
+          </div>
+        </div>
+        <div className="companydetails">
+          <p className="companylocation info">
+            <Skeleton />
+          </p>
+          <p className="timeperiod info">
+            <Skeleton />
+          </p>
+          <p className="stipend info">
+            <Skeleton />
+          </p>
+        </div>
+        <div className="btnlist">
+          <button className="secondary-btn more">
+            View
+          </button>
+          <a className="secondary-btn apply" target="_blank">
+            Apply
+          </a>
+        </div>
+      </div>
+      <div className="job-info">
+        <div className="card-heading">
+          <div className="header">
+            <p className="companyname">
+              <Skeleton />
+            </p>
+            <p className="job-desc">
+              <Skeleton />
+            </p>
+          </div>
+        </div>
+        <div className="companydetails">
+          <p className="companylocation info">
+            <Skeleton />
+          </p>
+          <p className="timeperiod info">
+            <Skeleton />
+          </p>
+          <p className="stipend info">
+            <Skeleton />
+          </p>
+        </div>
+        <div className="btnlist">
+          <button className="secondary-btn more">
+            View
+          </button>
+          <a className="secondary-btn apply" target="_blank">
+            Apply
+          </a>
+        </div>
+      </div>
+      <div className="job-info">
+        <div className="card-heading">
+          <div className="header">
+            <p className="companyname">
+              <Skeleton />
+            </p>
+            <p className="job-desc">
+              <Skeleton />
+            </p>
+          </div>
+        </div>
+        <div className="companydetails">
+          <p className="companylocation info">
+            <Skeleton />
+          </p>
+          <p className="timeperiod info">
+            <Skeleton />
+          </p>
+          <p className="stipend info">
+            <Skeleton />
+          </p>
+        </div>
+        <div className="btnlist">
+          <button className="secondary-btn more">
+            View
+          </button>
+          <a className="secondary-btn apply" target="_blank">
+            Apply
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
