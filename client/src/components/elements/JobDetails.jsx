3be9568cd2e68378ac44f83bb040ec132d2b2799
coder_17 <tanupamsaha26@gmail.com>
2023-04-07 10:12:25 +0530

    passing data from recent jobs to job details

--- client/src/components/elements/JobDetails.jsx
+++ client/src/components/elements/JobDetails.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react'
-
-export default function JobDetails(Details) {
+import React, { useState, useEffect } from 'react'
+import '../../assets/css/jobdetails.css'
+export default function JobDetails({ Details, Id }) {
 
   const [popup, setpopup] = useState(true)
   if (!popup) { return null }
-  const job = Details.Details
+  const jobs = Details
+  const [displayData, setdisplayData] = useState('')
+  useEffect(() => {
+    setdisplayData(Id);
+  }, [])
+
   return (
-    <div className='modal-bg'>
-      <div className='close'><button className='closebtn' onClick={() => {
-        setpopup(false)
-      }}
-      >X</button></div>
-      <div className="modal-details">
-        <div className="modal-header"></div>
+    jobs.data && (
+      <div className="modal">
+        {
+          jobs.data.filter((job) => {
+            return displayData === '' ? job : job._id.includes(displayData);
+          }).map((job) => (
+            <div className='modal-bg'>
+              <div className='close'><button className='closebtn' onClick={() => { setpopup(false) }}>X</button></div>
+              <div className="modal-details">
+                <div className="modal-header">{job.refInternshipTitle.name}</div>
+              </div>
+            </div>
+          ))
+        })
       </div>
-    </div>
-  )
+    ))
 }
