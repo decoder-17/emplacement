65b6a87d06d14f4ddddbb3ad01d347f35f962210
coder_17 <tanupamsaha26@gmail.com>
2023-04-15 22:16:25 +0530

    feature updation

--- client/src/components/sections/Features.jsx
+++ client/src/components/sections/Features.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Features() {
   return (
-    <div className="features"> <div className='heading'>
-      Features
-    </div></div>
-  )
+    <div className="features">
+      <div className="heading">Features</div>
+      <div className="points">
+        <div className="subheading">Easy to Use</div>
+        <div className="details">
+          Traversing through several websites for job and internship stops here.
+        </div>
+      </div>
+      <div className="points">
+        <div className="subheading">Easy to Use</div>
+        <div className="details">
+          No need to traverse through several websites no more.
+        </div>
+      </div>
+      <div className="points">
+        <div className="subheading">Easy to Use</div>
+        <div className="details">
+          No need to traverse through several websites no more.
+        </div>
+      </div>
+      <div className="points">
+        <div className="subheading">Easy to Use</div>
+        <div className="details">
+          No need to traverse through several websites no more.
+        </div>
+      </div>
+    </div>
+  );
 }
