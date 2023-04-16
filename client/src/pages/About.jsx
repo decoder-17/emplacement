304bb9b560a118ee219c35d0cbdcb0c5c86b57ff
coder_17 <tanupamsaha26@gmail.com>
2023-04-16 21:26:30 +0530

    updated about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,6 +7,33 @@ export default function About() {
       <div className="heading text sm:flex-1 my-2 py-2">
         The ones' who made it possible.
       </div>
+      <h1 className="content">Who actually worked</h1>
+      <div className="useful">
+        <div className="people">
+          <div className="image">
+            {" "}
+            <img
+              src="https://avatars.githubusercontent.com/u/55330971?v=4"
+              alt="avatar"
+              className="avatar"
+            />{" "}
+          </div>
+          <div className="name">Neeraj Chopra</div>
+          <div className="designation">Only Backend & Co.</div>
+        </div>
+        <div className="people">
+          <div className="image">
+            {" "}
+            <img
+              src="https://avatars.githubusercontent.com/u/54593072?v=4"
+              alt="avatar"
+              className="avatar"
+            />{" "}
+          </div>
+          <div className="name">Tanupam Saha</div>
+          <div className="designation">Enjoys depression</div>
+        </div>
+      </div>
     </div>
   );
 }
