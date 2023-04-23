fba7dea19bd6d386b195c8ce93910b06557f16dc
coder_17 <tanupamsaha26@gmail.com>
2023-04-23 12:53:48 +0530

    home navigation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import { Hero } from '../components/sections/Hero'
-import Footer from '../components/layout/Footer'
-import Sites from '../components/sections/Sites'
-import Features from '../components/sections/Features'
+import React, { useState } from "react";
+import { Hero } from "../components/sections/Hero";
+import Footer from "../components/layout/Footer";
+import Sites from "../components/sections/Sites";
+import Features from "../components/sections/Features";
+import { BsArrowRightCircleFill, BsArrowLeftCircleFill } from "react-icons/bs";
 
 export default function Home() {
+  const [change, setChange] = useState(0);
+
+  const changeComponent = (value) => {
+    setChange(value);
+  };
   return (
-    <>
-      <Hero />
-      <Features />
-      <Sites />
-    </>
-  )
+    <div className="Home">
+      <div className="change">
+        <div className="move-left">
+          <BsArrowLeftCircleFill
+            className="lefticon"
+            color="white"
+            size={40}
+            onClick={() => change !== 0 && changeComponent(change - 1)}
+          />
+        </div>
+        <div className="move-right">
+          <BsArrowRightCircleFill
+            className="righticon"
+            color="white"
+            size={40}
+            onClick={() => change !== 2 && changeComponent(change + 1)}
+          />
+        </div>
+      </div>
+      {change === 0 && <Hero />}
+      {change === 1 && <Features />}
+      {change === 2 && <Sites />}
+    </div>
+  );
 }
