cfb31ad19c8f183fc11c61334bb8f5f3f742819f
coder_17 <tanupamsaha26@gmail.com>
2023-05-13 09:50:23 +0530

    Final commit

--- client/src/components/sections/Sites.jsx
+++ client/src/components/sections/Sites.jsx
@@ -10,30 +10,30 @@ export default function Sites() {
     <div className={theme ? "dark" : ""}>
       <div className="hero bg-[#F6F6F9] dark:bg-[#181A1E] p-6">
         <div className="pages rounded-3xl bg-[#FFFFFF] dark:bg-[#202528] shadow-lg shadow-[rgba(132, 139, 200, 0.18)]-500/40">
-          <div className="heading">Pages</div>
-          <div className="sites">
-            <div className="site">
+          <div className="heading">Sources</div>
+          <div className="sites flex gap-4">
+            <div className="site bg-gray-300/90 p-8 rounded-full w-44 aspect-square  text-slate-900">
               <div className="sitelogo">
                 {" "}
                 <img src={cuvette} />{" "}
               </div>
               <a
                 href="https://cuvette.tech/app/student/"
-                className="sitename"
+                className="sitename text-gray-900"
                 target="_blank"
                 alt="cuvette"
               >
                 Cuvette
               </a>
             </div>
-            <div className="site">
+            <div className="site bg-gray-300/90 p-8 rounded-full w-44 aspect-square  text-slate-900">
               <div className="sitelogo">
                 {" "}
                 <img src={naukri} />{" "}
               </div>
               <a
                 href="https://www.naukri.com/"
-                className="sitename"
+                className="sitename text-gray-900"
                 target="_blank"
                 alt="naukri.com"
               >
